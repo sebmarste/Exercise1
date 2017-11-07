e456d2c204010e70b34ce9edccece8342c610031
sebastianstein <steins@in.tum.de>
2017-11-07 15:27:26 +0100

    added greeting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
-	}
+		System.out.println("Please enter your name:");
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String name = br.readLine();
+			HelloUser user = new HelloUser(name);
+			user.greetUser();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
